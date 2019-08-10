7beaf41d411ac288ce2897ffae67d69ad9320e45
Mehrad Rousta <mehrad77@gmail.com>
2019-08-10 14:47:07 +0430

    add axios and make first request to kutt servers

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,26 @@
 import * as vscode from 'vscode';
+import axios from 'axios';
+import { IUrl } from './types';
+const DOMAIN = 'https://kutt.it';
+const API_KEY = "DcJCiKE8bXI7oCdtszyuWzY4XThrueJvmtpFwQw0";
+
+axios.defaults.headers.common['X-API-Key'] = API_KEY;
 
 export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('extension.kutt', () => {
 		vscode.window.showInputBox().then(
 			(value) => {
-				vscode.window.showInformationMessage(value || "no link detected");
+				axios.post(`${DOMAIN}/api/url/submit`, {
+					target: "https://google.com",
+				})
+				.then(
+					res => {
+						const url:IUrl = res.data;
+						vscode.window.showInformationMessage(`Shorted link is ${url.shortUrl}`);
+					}
+				)
+				
 			},
 			(reason) => {
 				console.error("ERR001: Something bad happend.",reason)
